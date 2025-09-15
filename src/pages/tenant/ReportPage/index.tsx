72e0601e1ab665d8db73ad2a90552d11489c1f44
ryankzl <ryankzl001@gmail.com>
2025-09-15 17:37:51 +0800

    working code to display the past 7 days alarm counts (awaiting UI design)

--- src/pages/tenant/ReportPage/index.tsx
+++ src/pages/tenant/ReportPage/index.tsx
@@ -1,25 +1,19 @@
-import { Typography } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import Img from '~/assets/images/svg/repainr.svg';
-import ButtonCustom from '~/components/ButtonCustom';
+import AlarmSummary from './components/alarmSummary';
 
 export default function ReportPage() {
-  const navigate = useNavigate();
-  return <div className='flex items-center justify-center w-screen h-screen'>
-    <div className='flex flex-col items-center'>
-      <img alt='error' src={Img} className='max-w-[400px] max-h-[400px]' />
-      <div className='flex flex-col items-center justify-center mt-14'>
-        <Typography variant='h4'>Trang đang được bảo trì</Typography>
-        <ButtonCustom
-          variant='contained'
-          onClick={() => {
-            navigate(-1);
-          }}
-          className='!w-[120px] !h-[48px] !mt-6'
-        >
-          <Typography variant='button1'>Quay về</Typography>
-        </ButtonCustom>
+  return (
+    <div className="grid grid-cols-2 grid-rows-2 w-screen h-screen pt-[7vh] bg-[var(--bg)]">
+      {/* Quarter 1 */}
+      <div className="flex items-center justify-center border border-gray-700">
+        <div className="w-full h-full">
+          <AlarmSummary />
+        </div>
       </div>
+
+      {/* Keep the rest empty or remove if not needed */}
+      <div className="flex items-center justify-center border border-gray-700" />
+      <div className="flex items-center justify-center border border-gray-700" />
+      <div className="flex items-center justify-center border border-gray-700" />
     </div>
-  </div>;
+  );
 }
